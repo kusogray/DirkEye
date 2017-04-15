69ff1434c8ab130f258326262c02d17f18aaf176
kusogray <kusogray1@gmail.com>
2017-04-15 21:58:52 +0800

    first commit

---
+++ SnapEye/SnapEye/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapEye
+//
+//  Created by whmou on 2017/4/15.
+//  Copyright © 2017年 whmou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
