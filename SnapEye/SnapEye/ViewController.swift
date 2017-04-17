36c29a107bbb29bb619ec52adae3f793e2b4326b
kusogray <kusogray1@gmail.com>
2017-04-18 00:12:51 +0800

    fix keyboard issue

--- SnapEye/SnapEye/ViewController.swift
+++ SnapEye/SnapEye/ViewController.swift
@@ -10,10 +10,25 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate,
 UINavigationControllerDelegate {
+    
+    var isKeyboardExist = false;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        
+        //Uncomment the line below if you want the tap not interfere and cancel other interactions.
+        //tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+        self.view.frame.origin.y = 0
     }
     //拍照
     @IBAction func TakePhoto(sender: AnyObject)
@@ -29,21 +44,37 @@ UINavigationControllerDelegate {
             }
     }
     
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+ 
+    @IBOutlet weak var userDesInputField: UITextView!
+    
     @IBOutlet weak var textBottom: UITextView!
     
     func keyboardWillShow(notification: NSNotification) {
+        if (isKeyboardExist == false) {
         if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
             if self.view.frame.origin.y == 0{
                 self.view.frame.origin.y -= keyboardSize.height
+                isKeyboardExist = true
+
             }
+        }
         }
     }
     
     func keyboardWillHide(notification: NSNotification) {
+        if (isKeyboardExist == true){
+
         if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
             if self.view.frame.origin.y != 0{
                 self.view.frame.origin.y += keyboardSize.height
+                isKeyboardExist = false
             }
+        }
         }
     }
     
