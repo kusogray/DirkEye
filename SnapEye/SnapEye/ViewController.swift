15c462ba7115d1d763737cce0c7fe3bd825b6040
tmp <kusogray1@gmail.com>
2017-05-03 22:44:41 +0800

    use map as first view

--- SnapEye/SnapEye/ViewController.swift
+++ SnapEye/SnapEye/ViewController.swift
@@ -7,27 +7,79 @@
 //
 
 import UIKit
+import CoreLocation
 
-
-class ViewController: UIViewController{
+class ViewController: UIViewController, CLLocationManagerDelegate{
     
     
     
-    
+    let locationManager = CLLocationManager()
        
     
     
 
     override func viewDidLoad() {
+        
+        
+        
+        
+        
+        locationManager.delegate = self
+        
+        // 距離篩選器 用來設置移動多遠距離才觸發委任方法更新位置
+        locationManager.distanceFilter =
+        kCLLocationAccuracyNearestTenMeters
+        
+        // 取得自身定位位置的精確度
+        locationManager.desiredAccuracy =
+        kCLLocationAccuracyBest
+        
+        
+        // 首次使用 向使用者詢問定位自身位置權限
+        if CLLocationManager.authorizationStatus()
+            == .notDetermined {
+            // 取得定位服務授權
+            locationManager.requestWhenInUseAuthorization()
+            
+            // 開始定位自身位置
+            locationManager.startUpdatingLocation()
+        }
+            // 使用者已經拒絕定位自身位置權限
+        else if CLLocationManager.authorizationStatus()
+            == .denied {
+            // 提示可至[設定]中開啟權限
+            let alertController = UIAlertController(
+                title: "定位權限已關閉",
+                message:
+                "如要變更權限，請至 設定 > 隱私權 > 定位服務 開啟",
+                preferredStyle: .alert)
+            let okAction = UIAlertAction(
+                title: "確認", style: .default, handler:nil)
+            alertController.addAction(okAction)
+            self.present(
+                alertController,
+                animated: true, completion: nil)
+        }
+            // 使用者已經同意定位自身位置權限
+        else if CLLocationManager.authorizationStatus()
+            == .authorizedWhenInUse {
+            // 開始定位自身位置
+            locationManager.startUpdatingLocation()
+        }
+
+
         super.viewDidLoad()
+
+      
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "UserRequestMapViewController") as! UserRequestMapViewController
         
-        
-            }
-    
-    
-    
-    
+        self.present(vc, animated: true, completion: nil)
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
